ae2445e1cd2ab02a18f071d7ca21bb42b1106f4a
MonsieurClavett <alexmarcouiller9@gmail.com>
2025-12-13 23:57:55 -0500

    ajout bd + seed (shawi)

--- obj/Debug/net8.0-windows/Final.AssemblyInfo.cs
+++ obj/Debug/net8.0-windows/Final.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Final")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3854acd234c61ec28053c068624948005892050b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+856c1e232b297cc04c20cde8a555933a4873fac3")]
 [assembly: System.Reflection.AssemblyProductAttribute("Final")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Final")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
